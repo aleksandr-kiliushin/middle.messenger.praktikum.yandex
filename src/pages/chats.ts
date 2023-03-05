84fefe9cd85c6bdb88ec8b9d149a8a21dd08b213
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-03-05 10:10:56 +0400

    docs: Add links to deployed project and figma

--- src/pages/chats.ts
+++ src/pages/chats.ts
@@ -1,6 +1,6 @@
 import { sum } from "../utils/sum"
 
-const root = document.querySelector("#chat-body")
+const root = document.querySelector("#chats-body")
 if (root instanceof HTMLDivElement) {
   const calculationResult = sum(6, -1).toString()
   root.textContent = `6 + (-1) = ${calculationResult} (TypeScript works)`
