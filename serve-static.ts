06af0bd2fd1345be339d4586f26fb804daf4a6be
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-03-06 16:54:31 +0400

    feat: Allow routing for local development

--- serve-static.ts
+++ serve-static.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv"
 import express from "express"
+import path from "node:path"
+import fs from "node:fs"
 
 dotenv.config({ path: "./.env" })
 
@@ -8,6 +10,18 @@ const app = express()
 const PORT = process.env.EXPRESS_SERVER_PORT ?? "3000"
 
 app.use(express.static("./dist"))
+
+app.get("*", (req, res) => {
+  const correspondingHtmlFilePath = path.join(__dirname, "dist", req.url + ".html")
+  const pageNotFoundHtmlFilePath = path.join(__dirname, "dist", "404.html")
+
+  if (!fs.existsSync(correspondingHtmlFilePath)) {
+    res.sendFile(pageNotFoundHtmlFilePath)
+    return
+  }
+
+  res.sendFile(correspondingHtmlFilePath)
+})
 
 app.listen(PORT, () => {
   console.log(`Your app is running on http://localhost:${PORT}.`)
