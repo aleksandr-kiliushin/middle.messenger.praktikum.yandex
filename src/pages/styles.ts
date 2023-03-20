3c66e5574e4904d42821f7e41a2f0a33f7d371f8
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-03-20 08:53:44 +0400

    refactor: Styles import

--- src/pages/styles.ts
+++
@@ -1,7 +0,0 @@
-import "../styles/global.css"
-import "../styles/rows.css"
-
-import "../components/button/index.css"
-import "../components/message/index.css"
-import "../components/navigation/index.css"
-import "../components/chats-list-item/index.css"
