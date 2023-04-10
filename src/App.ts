2377d65f0572e33a774da65a8be1d60c549c8c75
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-04-10 19:18:18 +0400

    refactor: Use new Router in App

--- src/App.ts
+++ src/App.ts
@@ -3,7 +3,6 @@ import { Router } from "@utils/Router"
 import { ChangePassword } from "./pages/ChangePassword"
 import { Chats } from "./pages/Chats"
 import { InternalServerError } from "./pages/InternalServerError"
-import { PageNotFound } from "./pages/PageNotFound"
 import { Profile } from "./pages/Profile"
 import { Settings } from "./pages/Settings"
 import { SignIn } from "./pages/SignIn"
@@ -12,22 +11,13 @@ import { SignUp } from "./pages/SignUp"
 const router = new Router()
 
 export const App = () => {
-  router.use({ blockClass: Profile, pathname: "/profile" }).start()
-
-  switch (window.location.pathname) {
-    case "/change-password":
-      return new ChangePassword().markup
-    case "/messenger":
-      return new Chats().markup
-    case "/settings":
-      return new Settings().markup
-    case "/":
-      return new SignIn().markup
-    case "/sign-up":
-      return new SignUp().markup
-    case "/500":
-      return new InternalServerError().markup
-    default:
-      return new PageNotFound().markup
-  }
+  router
+    .use({ pageBlockClass: ChangePassword, pathname: "/change-password" })
+    .use({ pageBlockClass: Profile, pathname: "/profile" })
+    .use({ pageBlockClass: Chats, pathname: "/messenger" })
+    .use({ pageBlockClass: Settings, pathname: "/settings" })
+    .use({ pageBlockClass: SignIn, pathname: "/" })
+    .use({ pageBlockClass: SignUp, pathname: "/sign-up" })
+    .use({ pageBlockClass: InternalServerError, pathname: "/500" })
+    .start()
 }
