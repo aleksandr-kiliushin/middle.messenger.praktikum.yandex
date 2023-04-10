734b1786b46a472167a6a44222014af8935498f2
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-04-10 21:26:54 +0400

    fix: target -> currentTarget

--- src/utils/Router.ts
+++ src/utils/Router.ts
@@ -87,9 +87,9 @@ export class Router {
     }
 
     window.addEventListener("click", (event) => {
-      if (!(event.currentTarget instanceof HTMLElement)) return
+      if (!(event.target instanceof HTMLElement)) return
 
-      const clickedAnchor = event.currentTarget.closest("a")
+      const clickedAnchor = event.target.closest("a")
       if (!(clickedAnchor instanceof HTMLAnchorElement)) return
 
       event.preventDefault()
