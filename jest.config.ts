d1873c187dda22f544231088cee4b236ecdf2570
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-04-13 16:43:44 +0400

    chore: Add import aliases to jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,9 @@
 import { Config } from "jest"
 
 const config: Config = {
+  moduleNameMapper: {
+    "^@utils(.*)$": "<rootDir>/src/utils$1",
+  },
   preset: "ts-jest",
   testEnvironment: "node",
 }
