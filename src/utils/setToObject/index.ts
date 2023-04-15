f7dad1b0b049a87e397c2a8a6e2644a04a24d8df
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-04-15 20:49:00 +0400

    refactor: Remove redundant utils

--- src/utils/setToObject/index.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { deepMergeObjects } from "@utils/deepMergeObjects"
-
-export const setToObject = ({ object, keyPath, value }: { object: Record<string, any>; keyPath: string; value: any }) => {
-  const result = keyPath.split(".").reduceRight((acc, key) => {
-    return { [key]: acc }
-  }, value)
-  return deepMergeObjects(object, result)
-}
