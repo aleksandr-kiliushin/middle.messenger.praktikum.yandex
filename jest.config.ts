2dabf899c43071de1984a67826ee3b77d3877e2c
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-04-18 20:57:31 +0400

    chore: Add import aliases to jest config

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,14 @@ import { Config } from "jest"
 
 const config: Config = {
   moduleNameMapper: {
+    "^@api(.*)$": "<rootDir>/src/api$1",
+    "^@components(.*)$": "<rootDir>/src/components$1",
+    "^@constants(.*)$": "<rootDir>/src/constants$1",
+    "^@controllers(.*)$": "<rootDir>/src/controllers$1",
+    "^@cypress(.*)$": "<rootDir>/src/cypress$1",
+    "^@pages(.*)$": "<rootDir>/src/pages$1",
+    "^@store(.*)$": "<rootDir>/src/store$1",
+    "^@types(.*)$": "<rootDir>/src/types$1",
     "^@utils(.*)$": "<rootDir>/src/utils$1",
   },
   preset: "ts-jest",
