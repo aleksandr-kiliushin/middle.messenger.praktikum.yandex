f88b70acb58c28e2007be4e8ab731a646ce91d89
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-04-24 09:24:01 +0400

    chore: Uninstall parcel

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     node: true,
   },
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  ignorePatterns: ["dist", "cypress", "cypress.config.ts", "node_modules", ".parcel-cache"],
+  ignorePatterns: ["dist", "cypress", "cypress.config.ts", "node_modules"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 13,
